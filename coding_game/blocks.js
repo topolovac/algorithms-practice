59b9587612dcdbbc4833efd9ba1b61c13ce50ab8
Matej Topolovac <>
2023-12-06 12:27:03 +0100

    added correct blocks solution

--- coding_game/blocks.js
+++ coding_game/blocks.js
@@ -1,47 +1,54 @@
 function solve(width, height, nbBlocks, grid) {
-    // Convert grid strings to arrays for easier manipulation
-    grid = grid.map(row => row.split(''));
-
-    // Helper function to check if a block can move in a given direction
-    function canMove(block, direction) {
-        let positions = [];
-        let emptyRowOrColumn;
-
-        // Find all positions of the block
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                if (grid[y][x] === block) {
-                    positions.push({x, y});
-                }
-            }
-        }
-
-        // Check if all positions of the block can move in the given direction
-        return positions.every(pos => {
-            switch (direction) {
-                case 'UP':
-                    return pos.y === 0 || (grid[pos.y - 1][pos.x] !== 'X' && grid[pos.y - 1][pos.x] !== block);
-                case 'RIGHT':
-                    return pos.x === width - 1 || (grid[pos.y][pos.x + 1] !== 'X' && grid[pos.y][pos.x + 1] !== block);
-                case 'DOWN':
-                    return pos.y === height - 1 || (grid[pos.y + 1][pos.x] !== 'X' && grid[pos.y + 1][pos.x] !== block);
-                case 'LEFT':
-                    return pos.x === 0 || (grid[pos.y][pos.x - 1] !== 'X' && grid[pos.y][pos.x - 1] !== block);
-            }
-        });
+  const directions = {
+    UP: [0, -1],
+    DOWN: [0, 1],
+    LEFT: [-1, 0],
+    RIGHT: [1, 0],
+  };
+  for (let i = 0; i < nbBlocks; i++) {
+    if (!existsInGrid(grid, i)) {
+      continue;
     }
-
-    // Iterate through the grid to find blocks and check their movability
-    for (let block = 0; block < nbBlocks; block++) {
-        const blockStr = block.toString();
-        const directions = ['UP', 'RIGHT', 'DOWN', 'LEFT'];
-        for (let direction of directions) {
-            if (canMove(blockStr, direction)) {
-                return `${blockStr} ${direction}`;
-            }
-        }
+    for (let key in directions) {
+      if (canMove(grid, width, height, i, directions[key])) {
+        return i + " " + key;
+      }
     }
+  }
+  return "0 UP";
+}
 
-    // Return a message if no move is possible
-    return "No move possible";
+function existsInGrid(grid, block) {
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (Number(grid[i][j]) === block) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function canMove(grid, width, height, block, direction) {
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (Number(grid[i][j]) === block) {
+        exists = true;
+        if (!move(grid, direction, block, j, i)) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+function move(grid, vector, block, x, y) {
+  if (x < 0 || x >= grid[0].length || y < 0 || y >= grid.length) {
+    return true;
+  }
+  if (grid[y][x] === String(block) || grid[y][x] === ".") {
+    return move(grid, vector, block, x + vector[0], y + vector[1]);
+  }
+  return false;
 }
